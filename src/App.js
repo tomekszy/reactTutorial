0d5bee5695f91ad44ce1a61af75628954a1e9a5f
tomekszy <red_rose@poczta.fm>
2021-07-27 11:25:56 +0200

    props

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import { Greet } from './components/Greet';
 import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 
 function App() {
   return (
     <div className="App">
-      <Greet />
-      <Welcome />
+      <Greet name="Bruce" heroName="Batman"><p>This is children props</p></Greet>
+      <Greet name="Clark" heroName="Superman"><button>Action</button></Greet>
+      <Greet name="Diana" heroName="Wonder Woman" />
+      <Welcome name="Bruce" heroName="Batman"><p>This is children props</p></Welcome>
+      <Welcome name="Clark" heroName="Superman"><button>Action</button></Welcome>
+      <Welcome name="Diana" heroName="Wonder Woman" />
     </div>
   );
 }
